b24de6cedf423c6ca4c741c07e32f90024829dec
aribaldi <aribaldi@aribaldi.localdomain>
2019-12-06 12:43:55 +0300

    parallel block correction

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <omp.h>
-#include <ctime>
 #include <random>
 #include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
-int64_t **get_random_matrix(unsigned r, unsigned c) {
-    int64_t **random_matrix = new int64_t *[r];
+int32_t **get_random_matrix(unsigned r, unsigned c) {
+    auto **random_matrix = new int32_t *[r];
     std::random_device rd;
     std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(1.0, 150.0);
+    std::uniform_real_distribution<double> dist(1.0, 10000);
     //srand(time(NULL));
-    for (int64_t i = 0; i < r; ++i) {
-        random_matrix[i] = new int64_t[c];
-        for (int64_t j = 0; j < c; ++j) {
-            random_matrix[i][j] = dist(mt);
+    for (int32_t i = 0; i < r; ++i) {
+        random_matrix[i] = new int32_t[c];
+        for (int32_t j = 0; j < c; ++j) {
+            random_matrix[i][j] = static_cast<int32_t>(dist(mt));
         }
 
     }
@@ -24,39 +23,40 @@ int64_t **get_random_matrix(unsigned r, unsigned c) {
 }
 
 int main() {
-    const unsigned rows = 30000;
-    const unsigned cols = 30000;
+    const unsigned rows = 15000;
+    const unsigned cols = 15000;
 
-    int64_t **testmat = get_random_matrix(rows, cols);
+    int32_t **testmat = get_random_matrix(rows, cols);
 
 
-    int64_t global_maximum = -1;
+    int32_t global_maximum = -1;
     auto start = high_resolution_clock::now();
-    int64_t row_minimum = INT64_MAX;
-    #pragma omp_set_numthreads(4);
-    #pragma omp parallel for reduction(min: row_minimum)
-    for (int64_t l = 0; l < rows; ++l) {
-
-        for (int64_t i = 0; i < cols; ++i) {
+    int32_t row_minimum = INT32_MAX;
+    omp_set_num_threads(4);
+    #pragma omp_schedule(auto)
+    #pragma omp parallel for shared(testmat) firstprivate(row_minimum) default(none) reduction(max: global_maximum)
+    for (int32_t l = 0; l < rows; ++l) {
+        for (int32_t i = 0; i < cols; ++i) {
             if (testmat[l][i] < row_minimum) { row_minimum = testmat[l][i]; }
         }
         if (row_minimum > global_maximum) { global_maximum = row_minimum; }
-        row_minimum = INT64_MAX;
+        row_minimum = INT32_MAX;
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
-    cout << duration.count() << endl;
+    cout << "Elapsed time (milliseconds):" << duration.count() << endl;
 
-//    for (int64_t i = 0; i < rows; ++i) {
-//        for (int64_t j = 0; j < cols; ++j) {
+
+//    for (int32_t i = 0; i < rows; ++i) {
+//        for (int32_t j = 0; j < cols; ++j) {
 //            cout << testmat[i][j] << '\t';
 //        }
 //        cout << '\n';
 //    }
 
-    cout << global_maximum << endl;
+    cout << "Maximum of minimums:" << global_maximum << endl;
 
-    for (int64_t k = 0; k < rows; ++k) {
+    for (int32_t k = 0; k < rows; ++k) {
         delete[] testmat[k];
     }
     delete[] testmat;
